1b199e4d4e06084130927f56c766c7415dcb0405
root <root@ip-172-31-90-111.ec2.internal>
2024-07-02 17:39:00 +0000

    updates D1

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import os
 
 input_folder = '/path/to/images'
 output_folder = '/path/to/resized_images'
-desired_size = (100, 100)
+desired_size = (100, 200)
 
 for filename in os.listdir(input_folder):
     with Image.open(os.path.join(input_folder, filename)) as img:
